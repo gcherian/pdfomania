4a85cf2bbd89cc0ec3ab650c6122365ae2debe90
George Cherian <george.vengal@gmail.com>
2025-09-24 22:12:14 -0400

    Update cite.config.ts

--- cite.config.ts
+++ cite.config.ts
@@ -1,8 +1,5 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  server: { port: 5173 },
-  optimizeDeps: {
-    include: ["pdfjs-dist/build/pdf.worker.min.mjs"]
-  }
+  server: { port: 5173 }
 });
